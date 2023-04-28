8ce30a54507fcedb171c000f6b7ca3d2c47104b4
romekbulogin <roman.bulyg@gmail.com>
2023-04-28 22:04:16 +0300

    Disable CSRF

--- Dataquire-Gateway/src/main/kotlin/ru/dataquire/dataquiregateway/configuration/ReactiveWebfluxSecurityConfiguration.kt
+++ Dataquire-Gateway/src/main/kotlin/ru/dataquire/dataquiregateway/configuration/ReactiveWebfluxSecurityConfiguration.kt
@@ -1,19 +1,37 @@
 package ru.dataquire.dataquiregateway.configuration
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 
 @EnableWebFluxSecurity
+@Configuration
 class ReactiveWebfluxSecurityConfiguration {
 
     @Bean
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
         http
-            .csrf { csrf -> csrf.disable() }
+            .csrf().disable()
+            .cors().disable()
         return http.build()
+    }
+
+    @Bean
+    fun corsWebFilter(): CorsWebFilter? {
+        val corsConfig = CorsConfiguration()
+        corsConfig.allowedOrigins = listOf("*")
+        corsConfig.maxAge = 3600L
+        corsConfig.addAllowedMethod("*")
+        corsConfig.addAllowedHeader("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfig)
+        return CorsWebFilter(source)
     }
 }
