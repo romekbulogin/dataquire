808bbc90bc449a100b8a1e34c6dbaebb5a08d2bb
romekbulogin <roman.bulyg@gmail.com>
2023-04-28 22:44:38 +0300

    New CORS Configuration

--- Dataquire-Gateway/src/main/kotlin/ru/dataquire/dataquiregateway/configuration/ReactiveWebfluxSecurityConfiguration.kt
+++ Dataquire-Gateway/src/main/kotlin/ru/dataquire/dataquiregateway/configuration/ReactiveWebfluxSecurityConfiguration.kt
@@ -2,38 +2,34 @@ package ru.dataquire.dataquiregateway.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
-import org.springframework.security.config.web.server.ServerHttpSecurity
-
-import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.reactive.CorsWebFilter
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
 
 
-@EnableWebFluxSecurity
 @Configuration
-class ReactiveWebfluxSecurityConfiguration {
-
-    @Bean
-    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
-        http
-            .csrf().disable()
-            .cors().disable()
-        return http.build()
+@EnableWebFlux
+class ReactiveWebfluxSecurityConfiguration: WebFluxConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowCredentials(true)
+            .allowedOrigins("*")
+            .allowedHeaders("*")
+            .allowedMethods("*")
     }
 
     @Bean
     fun corsWebFilter(): CorsWebFilter? {
-        val corsConfig = CorsConfiguration()
-        corsConfig.allowedOrigins = listOf("*", "**", "http://localhost:3000/", "http://localhost:3000")
-        corsConfig.addAllowedMethod(HttpMethod.GET)
-        corsConfig.addAllowedMethod(HttpMethod.POST)
-        corsConfig.addAllowedHeader("*")
-        corsConfig.addAllowedHeader("**")
-        val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", corsConfig)
-        return CorsWebFilter(source)
+        val corsConfiguration = CorsConfiguration()
+        corsConfiguration.allowCredentials = true
+        corsConfiguration.addAllowedHeader("*")
+        corsConfiguration.addAllowedMethod("*")
+        corsConfiguration.addAllowedOrigin("*")
+        val corsConfigurationSource = UrlBasedCorsConfigurationSource()
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration)
+        return CorsWebFilter(corsConfigurationSource)
     }
 }
