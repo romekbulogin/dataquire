c50de4d0780c1f64e175d54ab6d4824af1d32a03
romekbulogin <roman.bulyg@gmail.com>
2023-04-28 23:57:06 +0300

    CORS Configuration

--- Dataquire-Gateway/src/main/kotlin/ru/dataquire/dataquiregateway/configuration/GatewayConfiguration.kt
+++ Dataquire-Gateway/src/main/kotlin/ru/dataquire/dataquiregateway/configuration/GatewayConfiguration.kt
@@ -4,6 +4,8 @@ import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
 
 @Configuration
 class GatewayConfiguration(private val authenticationFilter: AuthenticationFilter) {
@@ -21,5 +23,12 @@ class GatewayConfiguration(private val authenticationFilter: AuthenticationFilte
                     .uri("lb://authorization-service")
             }
             .build()
+    }
+
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
+        http
+            .csrf { csrf -> csrf.disable() }
+        return http.build()
     }
 }
