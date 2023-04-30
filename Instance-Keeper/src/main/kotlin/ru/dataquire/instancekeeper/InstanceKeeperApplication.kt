375cac369f79ea19a98827a1e2205f9f80d4b1d0
romekbulogin <roman.bulyg@gmail.com>
2023-04-30 23:55:42 +0300

    Resolve error with Table Manager and Instance Keeper

--- Instance-Keeper/src/main/kotlin/ru/dataquire/instancekeeper/InstanceKeeperApplication.kt
+++ Instance-Keeper/src/main/kotlin/ru/dataquire/instancekeeper/InstanceKeeperApplication.kt
@@ -2,8 +2,10 @@ package ru.dataquire.instancekeeper
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class InstanceKeeperApplication
 
 fun main(args: Array<String>) {
