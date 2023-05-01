e74055b9a86dbe9a4d8f2bebe6fa1acdcd300239
romekbulogin <roman.bulyg@gmail.com>
2023-05-01 12:20:15 +0300

    Added logger in Table Manager

--- Table-Manager/src/main/kotlin/ru/dataquire/tablemanager/controller/TableController.kt
+++ Table-Manager/src/main/kotlin/ru/dataquire/tablemanager/controller/TableController.kt
@@ -1,5 +1,7 @@
 package ru.dataquire.tablemanager.controller
 
+import mu.KotlinLogging
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import ru.dataquire.tablemanager.request.CreateTableRequest
 import ru.dataquire.tablemanager.request.ViewTableRequest
@@ -8,10 +10,13 @@ import ru.dataquire.tablemanager.service.TableManagerService
 @RestController
 @RequestMapping("/api/tables")
 class TableController(private val tableManagerService: TableManagerService) {
+    private val logger = KotlinLogging.logger { }
 
     @PostMapping("/view")
-    fun viewTable(@RequestHeader("Authorization") token: String, @RequestBody request: ViewTableRequest) =
-        tableManagerService.viewTable(token, request)
+    fun viewTable(@RequestHeader("Authorization") token: String, @RequestBody request: ViewTableRequest): Any {
+        logger.info("Request for view table: $request")
+        return tableManagerService.viewTable(token, request)
+    }
 
     @PostMapping("/create")
     fun createTable(@RequestHeader("Authorization") token: String, @RequestBody request: CreateTableRequest) =
@@ -24,13 +29,15 @@ class TableController(private val tableManagerService: TableManagerService) {
     fun getSqlDateTypes() = tableManagerService.getDefaultDateTypes()
 
     @PostMapping("/keys")
-    fun test(@RequestHeader("Authorization") token: String, @RequestBody request: ViewTableRequest) =
+    fun getKeysColumn(@RequestHeader("Authorization") token: String, @RequestBody request: ViewTableRequest) =
         tableManagerService.getColumnForForeignKey(token, request)
 
     @GetMapping("/{dbms}/{systemName}/{table}/structure")
     fun getColumnsOfTable(
         @RequestHeader("Authorization") token: String, @PathVariable systemName: String,
         @PathVariable dbms: String, @PathVariable table: String
-    ) =
-        tableManagerService.getColumnsOfTable(token, systemName, table)
+    ): ResponseEntity<Any> {
+        logger.info("Request for view table: $systemName $dbms $table")
+        return tableManagerService.getColumnsOfTable(token, systemName, table)
+    }
 }
